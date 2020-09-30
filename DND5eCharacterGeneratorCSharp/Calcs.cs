431e25e1a935aacc3fc96610b088032e957dd7d1
Proto <66826426+Protorun@users.noreply.github.com>
2020-09-30 05:23:27 +0100

    Started making calculations

---
+++ DND5eCharacterGeneratorCSharp/Calcs.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+public class Calcs
+{
+	public Calcs()
+	{
+		
+	}
+
+	public static int RollDice(int MaxValue)
+    {
+		var DiceRoller = new Random();
+		var Result = DiceRoller.Next(1, MaxValue);
+		return Result;
+    }
+
+	public static int CalcMod(int Attribute)
+    {
+		int Modifier;
+		if(Attribute <= 1)
+        {
+			Modifier = -5;
+        }
+		if (Attribute <= 3)
+		{
+			Modifier = -4;
+		}
+		if (Attribute <= 5)
+		{
+			Modifier = -3;
+		}
+		if (Attribute <= 7)
+		{
+			Modifier = -2;
+		}
+		if (Attribute <= 9)
+		{
+			Modifier = -1;
+		}
+		if (Attribute <= 11)
+		{
+			Modifier = 0;
+		}
+		if (Attribute <= 13)
+		{
+			Modifier = 1;
+		}
+		if (Attribute <= 15)
+		{
+			Modifier = 2;
+		}
+		if (Attribute <= 17)
+		{
+			Modifier = 3;
+		}
+		if (Attribute <= 19)
+		{
+			Modifier = 4;
+		}
+		if (Attribute == 20)
+		{
+			Modifier = 5;
+		}
+		else
+		{
+			Modifier = 0;
+		}
+
+		return Modifier;
+    }
+}
--- DND5eCharacterGeneratorCSharp/Character.cs
+++ DND5eCharacterGeneratorCSharp/Character.cs
@@ -6,6 +6,20 @@
 public class Character
 {
 	public String FirstName, LastName, Age, Height, Weight, Race, Class, Background;
+	public int HitPoints,
+		Strength,
+		Dexterity,
+		Constitution,
+		Intelligence,
+		Wisdom,
+		Charisma,
+		StrMod,
+		DexMod,
+		ConMod,
+		IntMod,
+		WisMod,
+		ChaMod,
+		Level;
 
 	public Character(String ToonFirstName, String ToonLastName, String ToonAge, String ToonHeight, String ToonWeight, String ToonRace, String ToonClass, String ToonBackground)
 	{
@@ -17,6 +31,74 @@ public class Character
 		Race = ToonRace;
 		Class = ToonClass;
 		Background = ToonBackground;
+		HitPoints = RollHP(Class);
+		Level = 1;
 	}
+
+	private int RollHP(String ClassName)
+    {
+		int Result;
+		if(ClassName == "Barbarian")
+        {
+            Result = Calcs.RollDice(12);
+        }
+		if(ClassName == "Bard")
+        {
+			Result = Calcs.RollDice(8);
+        }
+		if (ClassName == "Cleric")
+		{
+			Result = Calcs.RollDice(8);
+		}
+		if (ClassName == "Druid")
+		{
+			Result = Calcs.RollDice(8);
+		}
+		if (ClassName == "Fighter")
+		{
+			Result = Calcs.RollDice(10);
+		}
+		if (ClassName == "Monk")
+		{
+			Result = Calcs.RollDice(8);
+		}
+		if (ClassName == "Paladin")
+		{
+			Result = Calcs.RollDice(10);
+		}
+		if (ClassName == "Ranger")
+		{
+			Result = Calcs.RollDice(10);
+		}
+		if (ClassName == "Rogue")
+		{
+			Result = Calcs.RollDice(8);
+		}
+		if (ClassName == "Sorcerer")
+		{
+			Result = Calcs.RollDice(6);
+		}
+		if (ClassName == "Warlock")
+		{
+			Result = Calcs.RollDice(8);
+		}
+		if (ClassName == "Wizard")
+		{
+			Result = Calcs.RollDice(6);
+		}
+        else
+        {
+			Result = 0;
+        }
+		return Result;
+    }
+
+	public String LevelUp(String ClassName)
+    {
+		HitPoints = HitPoints + RollHP(ClassName);
+		String Confirm = ("HP roll for level up = " + HitPoints);
+		return Confirm;
+    }
+
 
 }
